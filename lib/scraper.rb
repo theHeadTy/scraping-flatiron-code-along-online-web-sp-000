a830bf6e9a8a152f155618077008ff9494d8dc8c
generic-puppet-7755 <tasmith1990@gmail.com>
2020-02-04 23:32:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  
+ def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+    binding.pry
+  end
   
   def print_courses
     self.make_courses
@@ -20,3 +27,4 @@ end
 
 
 
+Scraper.new.get_page
